dcb670732070406af2553eb33206ca60e9f9426e
kefah123 <kefah@uvm.edu>
2020-04-03 12:04:15 -0400

    Implemented cook and station classes

---
+++ Cook.py
@@ -0,0 +1,56 @@
+from Station import Station
+
+
+class Cook:
+    def __init__(self, name, age, wage, shift, id_no, station_name):
+        self.name = name
+        self.age = age
+        self.wage = wage
+        self.shift = shift
+        self.id_no = id_no
+        self.station = Station(station_name)
+
+    def get_id_no(self):
+        return self.id_no
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def get_wage(self):
+        return self.wage
+
+    def get_shift(self):
+        return self.shift
+
+    def get_station(self):
+        return self.station
+
+    def set_station(self, station_name):
+        self.station = Station(station_name)
+
+    def set_id_no(self, id_no):
+        self.id_no = id_no
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_age(self, age):
+        self.age = age
+
+    def set_wage(self, wage):
+        self.wage = wage
+
+    def set_shift(self, shift):
+        self.shift = shift
+
+    def to_string(self):
+        s = "Name:", self.name + "\n"
+        s += "ID:", self.id_no + "\n"
+        s += "Age:", self.age + "\n"
+        s += "Wage:", self.wage + "\n"
+        s += "Shift:", self.shift + "\n"
+        s += "Station:", self.station.name
+        return s
---
+++ Station.py
@@ -0,0 +1,40 @@
+class Station:
+    def __init__(self, name):
+        self.name = name
+        self.dishes = {}
+        self.difficulty = ""
+
+    def get_name(self):
+        return self.name
+
+    def get_dishes(self):
+        return self.dishes
+
+    def get_difficulty(self):
+        return self.difficulty
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_difficulty(self, difficulty):
+        self.difficulty = difficulty
+
+    def update_dish(self, dish, ingredients):
+        self.dishes[dish] = ingredients
+
+    def get_dish_ingredients(self, dish):
+        return self.dishes[dish]
+
+    def find_dish(self, ingredient):
+        found_dishes = []
+        for dish in self.dishes:
+            if ingredient in self.dishes[dish]:
+                found_dishes.append(dish)
+
+        return found_dishes
+
+    def to_string(self):
+        s = "Station: " + self.name + "\nDifficulty: " + self.difficulty + "\nDishes:\n"
+        for dish, ingredient in self.dishes.items():
+            s += dish + ": " + ingredient
+        return s
