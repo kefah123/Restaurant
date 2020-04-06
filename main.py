29452632d94d7923e36489bd5b5d448aded1f8fb
kefah123 <kefah@uvm.edu>
2020-04-05 22:42:03 -0400

    finished implementing main program

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     the_spot.update_station_dish("Apps", "Fried Calamari",
                                  ("Frozen Calamari", "Scallions", "Black Sesame Seeds", "Ponzu Sauce"))
     the_spot.update_station_dish("Apps", "Avocado Fries",
-                                 ("Frozen breaded avocado slices", "Sweet Chili Aioli"))
+                                 ("Frozen breaded avocado slices", "Sweet Chili Aioli", "Lime Wedge"))
     the_spot.update_station_dish("Saute", "Ahi Tuna",
                                  ("Raw Ahi Tuna", "Scallions", "Black Sesame Seeds", "Green Goddess Dressing",
                                   "Fried rice"))
@@ -40,6 +40,10 @@ def main():
     the_spot.update_station_dish("Cold", "Cloudbreak Salad",
                                  ("Mixed greens", "Black beans", "Tomatoes", "Eggs", "Chicken", "Goat Cheese"))
 
+    # Outputs to demonstrate functionality
+
+    print("WELCOME TO THE SPOT ON THE DOCK!\n")
+
     print("OUR TEAM FOR THIS YEAR IS MADE UP OF:\n")
     for cook in the_spot.get_cooks():
         print(cook.to_string() + "\n")
@@ -47,6 +51,10 @@ def main():
     print("WE WILL BE RUNNING THE FOLLOWING STATIONS:\n")
     for station in the_spot.get_stations():
         print(station.to_string() + "\n")
+
+    print("WE ARE CURRENTLY LOW ON BLACK SESAME SEEDS. THESE ARE THE DISHES THAT USE THEM: ")
+    for dish in the_spot.find_dish_using_ingredient("Black Sesame Seeds"):
+        print(dish)
 
 
 main()
